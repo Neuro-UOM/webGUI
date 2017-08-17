a0c448fc484987f327de75ad01278f8640d34e5a
dinuka <dinukasal@gmail.com>
2017-08-17 15:09:37 +0530

    pushing from salas lap

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+from IPython import get_ipython
+import seaborn as sns
+import glob
+import pandas as pd
+from scipy import signal
+
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
@@ -16,6 +25,34 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 
+    # extracted from : https://stackoverflow.com/questions/39032325/python-high-pass-filter
+def butter_highpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
+    return b, a
+
+def butter_highpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_highpass(cutoff, fs, order=order)
+    y = signal.filtfilt(b, a, data)
+    return y
+
+def Fourier_Thread(O2_array):
+    # specifying the O2 node for the value
+    y = O2_array
+    y = butter_highpass_filter(y,5,132,5)
+
+    ps = np.abs(np.fft.fft(y))**2
+
+    time_step = float(1)/128
+    freqs = np.fft.fftfreq( y.size , time_step )
+    idx = np.argsort(freqs)
+    
+    socketio.emit('fourier_response',{'data': 'Server generated event', 'freq': freqs , 'ps':ps, 'idx': idx }, namespace='/test')
+
+    # return freqs[idx] , ps[idx]
+
+
 def collect_raw_thread():
 
     """ collect raw data from another thread"""
@@ -23,14 +60,25 @@ def collect_raw_thread():
     sensor_names = ["T7","T8","P7","P8","O1","O2"]
 
     with Emotiv(display_output=True, write=True, write_decrypted=True, verbose=True, output_path= "data") as headset:
+        O2_array = []
         while True:
             packet = headset.dequeue()
             sensor_vals = []
             if packet is not None:
                 for i in sensor_names:
                     sensor_vals.append(dict(packet.sensors)[i]['value'])
+                
+                O2_array.append(sensor_vals[5])
+                
                 socketio.sleep(0.1)
+
+                # if len(O2_array) > 128:
+                #     socketio.emit('fourier_response',{'data': 'Server generated event' }, namespace='/test')
+                #     Fourier_Thread(O2_array)
+                    
                 arr = sensor_vals
+                
+                socketio.emit('fourier_response',{'data': 'Server generated event', 'data': O2_array }, namespace='/test')
                 socketio.emit('raw_response',{'data': 'Server generated event', 'raw_array': arr}, namespace='/test')
 
 
